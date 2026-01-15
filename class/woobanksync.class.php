cc62c36d76d36423db8ca6e38835a9b74febb377
sayehava <93396701+sayehava@users.noreply.github.com>
2026-01-15 10:57:00 +0100

    feat: add WooBankSync class skeleton with constructor

---
+++ class/woobanksync.class.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/woocommerceclient.class.php';
+
+class WooBankSync
+{
+    private $db;
+    private $conf;
+    private $langs;
+    public $errors = array();
+
+    public function __construct($db, $conf, $langs)
+    {
+        $this->db = $db;
+        $this->conf = $conf;
+        $this->langs = $langs;
+    }
+}
