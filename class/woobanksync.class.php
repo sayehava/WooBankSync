932fb1350dfc16bc4db60c80a450cdfa3a96d946
sayehava <93396701+sayehava@users.noreply.github.com>
2026-01-15 11:09:00 +0100

    feat: add client factory method to WooBankSync

--- class/woobanksync.class.php
+++ class/woobanksync.class.php
@@ -15,4 +15,13 @@ class WooBankSync
         $this->conf = $conf;
         $this->langs = $langs;
     }
+
+    public function client()
+    {
+        return new WbsWooCommerceClient(
+            $this->getConst('WBS_WOO_URL'),
+            $this->getConst('WBS_WOO_CONSUMER_KEY'),
+            $this->getConst('WBS_WOO_CONSUMER_SECRET')
+        );
+    }
 }
