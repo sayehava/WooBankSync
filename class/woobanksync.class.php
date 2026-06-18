ede682a6582fc61e4def070e2b99b2719f65e26e
sayehava <93396701+sayehava@users.noreply.github.com>
2026-06-18 16:06:20 +0200

    fix: add gzd, gzdp, document_data needles to invoice key discovery

--- class/woobanksync.class.php
+++ class/woobanksync.class.php
@@ -595,7 +595,7 @@ class WooBankSync
     private function discoverInvoiceKeys($orders)
     {
         $keys = array();
-        $needles = array('invoice', 'rechnung', 'germanized', 'wcpdf', 'pdf_invoice', 'document_number');
+        $needles = array('invoice', 'rechnung', 'germanized', 'gzd', 'gzdp', 'wcpdf', 'pdf_invoice', 'document_number', 'document_data');
         foreach ($orders as $order) {
             foreach (($order['meta_data'] ?? array()) as $meta) {
                 $key = (string) ($meta['key'] ?? '');
