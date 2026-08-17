123d5eeefda0e3d9866b7730d5acc6a938868dd4
sayehava <93396701+sayehava@users.noreply.github.com>
2026-08-17 14:42:12 +0200

    chore: add sync log upgrade columns

--- sql/llx_woobanksync_log.key.sql
+++ sql/llx_woobanksync_log.key.sql
@@ -1,2 +1,4 @@
 ALTER TABLE llx_woobanksync_log ADD COLUMN IF NOT EXISTS payout_amount double(24,8) DEFAULT 0;
 ALTER TABLE llx_woobanksync_log ADD COLUMN IF NOT EXISTS woo_invoice_number varchar(255) DEFAULT NULL;
+ALTER TABLE llx_woobanksync_log ADD COLUMN IF NOT EXISTS connector varchar(32) NOT NULL DEFAULT 'woocommerce';
+ALTER TABLE llx_woobanksync_log ADD COLUMN IF NOT EXISTS fee_source varchar(128) DEFAULT NULL;
